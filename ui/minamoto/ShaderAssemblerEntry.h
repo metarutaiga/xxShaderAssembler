de1ce131aea2b24c62f36f2bbca743bb6690dd2b
TAiGA <>
2025-02-25 14:53:38 +0800

    Update

--- ui/minamoto/ShaderAssemblerEntry.h
+++ ui/minamoto/ShaderAssemblerEntry.h
@@ -6,31 +6,25 @@
 //==============================================================================
 #pragma once
 
+#include <xxGraphic/xxGraphic.h>
+#include <Runtime/Runtime.h>
+
+#ifdef _WIN32
+#undef IMGUI_API
+#define IMGUI_API __declspec(dllimport)
+#endif
+
 #include <Interface.h>
+#include <Editor/ImGuiHelper.h>
+#include <Runtime/Tools/WindowsHeader.h>
+
+#ifdef _WIN32
+#undef IMGUI_API
+#define IMGUI_API
+#endif
 
 #include <assert.h>
 
 #include <functional>
 #include <string>
 #include <vector>
-
-#include <xxGraphic/xxGraphic.h>
-#include <Editor/ImGuiHelper.h>
-#include <Runtime/Tools/WindowsHeader.h>
-
-#define xxFourCC
-#define xxEightCC
-constexpr uint32_t operator ""_cc(char const* text, size_t length)
-{
-    uint32_t value = 0;
-    for (size_t i = 0; i < length; ++i)
-        value += uint32_t(uint8_t(text[i])) << (i * 8);
-    return value;
-};
-constexpr uint64_t operator ""_CC(char const* text, size_t length)
-{
-    uint64_t value = 0;
-    for (size_t i = 0; i < length; ++i)
-        value += uint64_t(uint8_t(text[i])) << (i * 8);
-    return value;
-};
